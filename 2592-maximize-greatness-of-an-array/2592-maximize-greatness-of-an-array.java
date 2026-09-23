699a1cba6d7f6608a35aa0379d5534758b49a13b
Aajadh Panigrahi <azadpanigrahi469@gmail.com>
2026-09-23 10:04:33 +0530

    Time: 17 ms (71.53%), Space: 75.4 MB (95.83%) - LeetHub

---
+++ 2592-maximize-greatness-of-an-array/2592-maximize-greatness-of-an-array.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maximizeGreatness(int[] nums) {
+            Arrays.sort(nums);
+            int i = 0;
+            int j = 0;
+            int count = 0;
+            while(j<nums.length){
+                if(nums[i]<nums[j]){
+                    count++;
+                    i++;
+                }
+                j++;
+            }
+            return count;
+    }
+}
